c800c95cd5e16c8d2ceaa400de122a151e568d6d
Mason <masonbe25@gmail.com>
2021-02-05 12:53:32 -0500

    Created Circle Class

---
+++ src/Circle.php
@@ -0,0 +1,24 @@
+<?php
+
+class Circle extends Shape{
+    const SHAPE_TYPE = 3;
+    protected $radius;
+
+    function __construct($radius){
+        $this->radius = $radius;
+
+        parent::__construct(0, 0);
+    }
+
+    public function area(){
+        return pi() * pow($this->radius, 2);
+    }
+
+    public function getFullDescription(){
+        return "Circle<#{$this->getId()}>: {$this->name} - {$this->radius}";
+    }
+
+
+}
+
+?>
