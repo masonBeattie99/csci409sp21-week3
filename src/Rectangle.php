bc36ab3bbbbcd6cc59ae5fc267227d6917436863
Mason <masonbe25@gmail.com>
2021-02-05 12:53:03 -0500

    Created Rectangle Class

---
+++ src/Rectangle.php
@@ -0,0 +1,13 @@
+<?php
+
+class Rectangle extends Shape{
+    const SHAPE_TYPE = 2;
+
+    public function getFullDescription(){
+        return "Rectangle<#{$this->getId()}>: {$this->name} - {$this->length} x {$this->width}";
+    }
+
+}
+
+?>
+
